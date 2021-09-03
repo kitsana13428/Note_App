97bc401a5311517e5ce7add1ba740875dc4dca0d
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-09-03 21:21:21 +0700

    Javascript

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const editBtn = document.querySelector('.edit');
-const deleteBtn = document.querySelector('.delete');
+const notesEl = document.querySelector(".notes");
+const editBtn = document.querySelector(".edit");
+const deleteBtn = document.querySelector(".delete");
+
+const main = notesEl.querySelector(".main");
+const textArea = notesEl.querySelector("textarea");
+
+editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+});
